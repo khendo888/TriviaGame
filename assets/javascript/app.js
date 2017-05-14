8977bcce046e7284ff4e4f42340ee4016da19392
Karen Henderson <Karen Henderson>
2017-05-14 12:21:59 -0400

    still working on js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,15 +16,20 @@ PSEUDO-CODE
  -------------------------------------
  1. on clicking the START button, the timer starts counting down 
  2. the questions and possible answers are unhidden
-
+ 3. the DONE button is unhidden
+ 4.
+*/
 // DEFINE GLOBAL VARIABLES
 //--------------------------------------------------------------------
 
 var correctAnswer =0;
 var incorrectAnswer=0;
 var unanswered=0;
-
-
+var hidden="#hiddenQuestions";
+var questions=["q1, q2, q3, q4, q5, q6, q7, q8, q9, q10"];
+//class - hiddenQuestions (questions hidden until start button clicked)
+//id-done (done button; hidden until start button pressed)
+//id-start (start button)
 
 
 
@@ -39,14 +44,13 @@ var unanswered=0;
 //when click START button, timer starts
 
 // creates a timer that counts down in minutes
-$(function(){
-	$("#ms_timer").countdowntimer({
-		minutes : 2‚
-                seconds : 0‚
-                size : "lg"
-	});
-});
-
+//$(function(clock){
+//	$("#ms_timer").countdowntimer({
+//		minutes : 2‚
+ //               seconds : 0‚
+   //             size : "lg"
+//	});
+//)};
 
 //restart the game with counters set to zero
 function restart () {
@@ -55,11 +59,41 @@ function restart () {
 	unanswered=0
 }
 
+function startGame() {
+	
+}
+
+function answerQuestion () {
+/*for (i=0, i<questions.length, i++) 
+	if value==="true"
+		correctAnswer=correctAnswer + 1;
+	else if value==="false"
+		incorrectAnswer=incorrectAnswer + 1;
+	else
+		unanswered=unanswered + 1;*/
+//}	
+
+//}
+
+function endGame() {
+	console.log("game over");
+
+
+}
+
+
+function displayScore(){
+	$(right).text(correctAnswersCounter);
+	$(wrong).text(wrongAnswersCounter);
+}
 
 //MAIN PROCESS
 //----------------------------------------------------------------------------
 //click START button to start game and timer countdown
-$("start").on("click", start) 
-
-
+//$("hidden").click("click", start) 
+ $("start").click(function(){
+        $("hiddenQuestions").show();
+});
+}
+//clock () 
 
